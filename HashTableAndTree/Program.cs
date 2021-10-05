72ff3236373be84ec684a2f2780714cd3ced3bc1
chaitanya4294 <chaitanya.4294@gmail.com>
2021-10-05 18:53:47 +0530

    [Chaitanya] Add : Added Hashtable

--- HashTableAndTree/Program.cs
+++ HashTableAndTree/Program.cs
@@ -6,7 +6,16 @@ namespace HashTableAndTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+
+            string hash2 = hash.GetV("2");
+            Console.WriteLine(hash2);
         }
     }
 }
